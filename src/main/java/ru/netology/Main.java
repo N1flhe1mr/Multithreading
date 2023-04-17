0ba9ed5904239c0b27f0dca6dd3d95d9742b5e90
Dmitriy_Korchagin <793070407232@ya.ru>
2023-04-17 16:32:10 +0300

    add threadPool && refactoring

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,52 +1,47 @@
 package ru.netology;
 
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
         List<Future<Integer>> futures = new ArrayList<>();
+
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
-        for (int l = 0; l < texts.length; l++) {
-            int target = l;
-            Callable<Integer> callableLogic = () -> {
-                int maxSize = 0;
-                String string = texts[target];
-                for (int i = 0; i < string.length(); i++) {
-                    for (int j = 0; j < string.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (string.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(string.substring(0, 100) + " -> " + maxSize);
-                return maxSize;
-            };
-            futures.add(new FutureTask<>(callableLogic));
-        }
+        int maxValue = 0;
+        ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
         long startTs = System.currentTimeMillis(); // start time
-        for (Future<Integer> future : futures) {
-            new Thread((Runnable) future).start();
+
+        for (String text : texts) {
+            futures.add(threadPool.submit(getMyCallable(text)));
         }
-        /* for (String text : texts) {
+
+        for (Future<Integer> future : futures) {
+            maxValue = Math.max(future.get(), maxValue);
+        }
+
+        threadPool.shutdown();
+        long endTs = System.currentTimeMillis(); // end time
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Max value: " + maxValue);
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
+    public static Callable<Integer> getMyCallable(String text) {
+        return () -> {
             int maxSize = 0;
             for (int i = 0; i < text.length(); i++) {
                 for (int j = 0; j < text.length(); j++) {
@@ -66,24 +61,7 @@ public class Main {
                 }
             }
             System.out.println(text.substring(0, 100) + " -> " + maxSize);
-        } */
-        int max = 0;
-        for (Future<Integer> future : futures) {
-            if (future.get() > max) {
-                max = future.get();
-            }
-        }
-        long endTs = System.currentTimeMillis(); // end time
-        System.out.println("Time: " + (endTs - startTs) + "ms");
-        System.out.println("Max: " + max);
-    }
-
-    public static String generateText(String letters, int length) {
-        Random random = new Random();
-        StringBuilder text = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
-        }
-        return text.toString();
+            return maxSize;
+        };
     }
 }
